7c2cf4e46d9a52201316fb1a9350561be1c0c3f5
GitHub Action <action@github.com>
2022-04-30 00:44:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/loccen/aictrltool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/loccen/aictrltool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.loccen.aictrltool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.loccen.aictrltool.MyBundle
 
 class MyApplicationService {
 
